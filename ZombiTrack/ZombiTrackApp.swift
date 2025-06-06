48b057ac2f91403368b3152705870ac6a7879196
Stephen Parker <parker1978@me.com>
2025-06-06 08:49:21 -0500

    Initial Commit

---
+++ ZombiTrack/ZombiTrackApp.swift
@@ -0,0 +1,32 @@
+//
+//  ZombiTrackApp.swift
+//  ZombiTrack
+//
+//  Created by Stephen Parker on 6/6/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ZombiTrackApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ ZombiTrackTests/ZombiTrackTests.swift
@@ -0,0 +1,17 @@
+//
+//  ZombiTrackTests.swift
+//  ZombiTrackTests
+//
+//  Created by Stephen Parker on 6/6/25.
+//
+
+import Testing
+@testable import ZombiTrack
+
+struct ZombiTrackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
